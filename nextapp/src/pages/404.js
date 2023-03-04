69d445a86774ebc160d1b8f466403b38b0773a20
MAXASSASSINS <mohammedshadab23april@gmail.com>
2023-03-04 23:10:12 +0530

    13 redirecting to home after few seconds

--- nextapp/src/pages/404.js
+++ nextapp/src/pages/404.js
@@ -1,11 +1,20 @@
 import React from 'react'
-
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 const ErrorPage = () => {
-  return (
-    <>
-        <h1>404 - Page Not Found</h1>
-    </>
-  )
+    const router = useRouter()
+    useEffect(() => {
+        setTimeout(() => {
+            router.push('/')
+        }, 3000)
+    }, [])
+
+
+    return (
+        <>
+            <h1>404 - Page Not Found</h1>
+        </>
+    )
 }
 
 export default ErrorPage
