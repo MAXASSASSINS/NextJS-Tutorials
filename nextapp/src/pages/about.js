dfb6702721fde1ad97edbe5a42919c436e15c8e8
MAXASSASSINS <mohammedshadab23april@gmail.com>
2023-03-04 23:06:43 +0530

    12 onclick redirecting

--- nextapp/src/pages/about.js
+++ nextapp/src/pages/about.js
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import { useRouter } from 'next/router'
 const about = () => {
+  const router = useRouter()
   return (
-    <div>about</div>
+    <div>
+      about
+      <button onClick={() => router.push('/')}>Back to Home</button>
+    </div>
   )
 }
 
