3e55538d651c8a08d1e8e9fcb0fe9645ddae712d
MAXASSASSINS <mohammedshadab23april@gmail.com>
2023-03-04 22:54:09 +0530

    navigating throught link component

--- nextapp/src/pages/index.js
+++ nextapp/src/pages/index.js
@@ -1,6 +1,22 @@
+import Link from "next/link";
+
 const Home = () => {
   return (
     <>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/about">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact">  
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </nav>
       <h1>Hello World</h1>
     </>
   )
