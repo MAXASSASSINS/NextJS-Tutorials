8235bbed21c6bf54f02595e2cad50b5162bdca43
MAXASSASSINS <mohammedshadab23april@gmail.com>
2023-03-04 22:42:52 +0530

    nested routing

---
+++ nextapp/src/pages/blog/blog1.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const blog1 = () => {
+  return (
+    <div>This is blog 1</div>
+  )
+}
+
+export default blog1
---
+++ nextapp/src/pages/blog/blog2.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const blog2 = () => {
+  return (
+    <div>This is blog 2</div>
+  )
+}
+
+export default blog2
