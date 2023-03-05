19a13bd6793d61d3399e66b1c07db0314a6ced5f
MAXASSASSINS <mohammedshadab23april@gmail.com>
2023-03-05 15:06:03 +0530

    19 Image component in next

--- nextapp/src/pages/index.js
+++ nextapp/src/pages/index.js
@@ -1,12 +1,18 @@
 import Navbar from "@/components/Navbar";
 import styles from '../styles/index.module.css'
+import Image from "next/image";
 
 const Home = () => {
   return (
     <div className={styles.main}>
       <Navbar />
       <h1 className={`${styles.red} ${styles.blue}`}>Hello World</h1>
+      <Image src="/vercel.svg" width={200} height={200} />
+      <Image src="https://images.pexels.com/photos/15417676/pexels-photo-15417676.jpeg"
+        width={200} height={200} alt="Picture of the author"
+      />
     </div>
+
   )
 }
 
